b0923508ebda98670ae060fecb7424be3a29a48d
Pim Clappers <pim@rometron.nl>
2024-12-02 09:23:32 +0100

    day 2 babyyyyyy

---
+++ 2024/day2/part1.py
@@ -0,0 +1,46 @@
+file = open('input.txt')
+lines = file.readlines()
+
+records = []
+
+totalSafeRecords = 0
+
+for line in lines:
+	splittedLine = line.split()
+	record = []
+	safe = False
+	decreasing = False
+	previousLevel = 0
+
+	if (int(splittedLine[0]) > int(splittedLine[1])):
+		decreasing = True
+		previousLevel = int(splittedLine[0]) + 1
+	else:
+		decreasing = False
+		previousLevel = int(splittedLine[0]) - 1
+
+	for levelString in splittedLine:
+		level = int(levelString)
+		record.append(level)
+
+		if (not decreasing):
+			if ((level > previousLevel) and (level - previousLevel < 4)):
+				safe = True
+			else:
+				safe = False
+				break
+		else:
+			if ((level < previousLevel) and (previousLevel - level < 4)):
+				safe = True
+			else:
+				safe = False
+				break
+
+		previousLevel = level
+
+	if (safe):
+		totalSafeRecords += 1
+
+	records.append(record)
+		
+print(totalSafeRecords)
